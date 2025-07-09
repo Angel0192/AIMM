3f5a399ce900293aee65027b0ca930d056d6cb00
Angel0192 <85460282+Angel0192@users.noreply.github.com>
2025-07-09 17:07:10 -0400

    Update arduino_code.ino

--- arduino/arduino_code.ino
+++ arduino/arduino_code.ino
@@ -1,56 +1,36 @@
-#include <micro_ros_arduino.h>
-#include <rcl/rcl.h>
-#include <rclc/rclc.h>
-#include <rclc/executor.h>
-#include <geometry_msgs/msg/twist.h>
-#include <ESP32Servo.h>
+#include <Servo.h>
 
 Servo leftMotor, rightMotor;
 
-int leftMotor_Pin = 10;
-int rightMotor_Pin = 9;
-
-rcl_subscription_t subscriber;
-geometry_msgs__msg__Twist msg;
-
-rclc_executor_t executor;
-rcl_allocator_t allocator;
-rclc_support_t support;
-rcl_node_t node;
-
-void subscription_callback(const void * msgin) {
-  const geometry_msgs__msg__Twist * msg = (const geometry_msgs__msg__Twist *)msgin;
-  float linear = msg->linear.x;
-  float angular = msg->angular.z;
-
-  int left_speed = (linear - angular) * 90 + 90;
-  int right_speed = (linear + angular) * 90 + 90;
-
-  leftMotor.write(left_speed);
-  rightMotor.write(right_speed);
-}
-
 void setup() {
-  set_microros_transports(); // Serial transport
-
-  leftMotor.attach(rightMotor_Pin);
-  rightMotor.attach(leftMotor_Pin);
-
-  allocator = rcl_get_default_allocator();
-  rclc_support_init(&support, 0, NULL, &allocator);
-  rclc_node_init_default(&node, "esp32_node", "", &support);
-
-  rclc_subscription_init_default(
-    &subscriber,
-    &node,
-    ROSIDL_GET_MSG_TYPE_SUPPORT(geometry_msgs, msg, Twist),
-    "cmd_vel"
-  );
-
-  rclc_executor_init(&executor, &support.context, 1, &allocator);
-  rclc_executor_add_subscription(&executor, &subscriber, &msg, &subscription_callback, ON_NEW_DATA);
+  Serial.begin(115200); // For ESP32, 115200 is typical
+  leftMotor.attach(9);   // PWM output to RC Input 1
+  rightMotor.attach(10); // PWM output to RC Input 2
+  Serial.println("ESP32 Ready");
 }
 
 void loop() {
-  rclc_executor_spin_some(&executor, RCL_MS_TO_NS(100));
+  if (Serial.available()) {
+    String command = Serial.readStringUntil('\n');
+    command.trim();
+
+    if (command == "Forward") {
+      leftMotor.writeMicroseconds(2000);
+      rightMotor.writeMicroseconds(2000);
+      Serial.println("Moving forward");
+    }
+    else if (command == "Backward") {
+      leftMotor.writeMicroseconds(1000);
+      rightMotor.writeMicroseconds(1000);
+      Serial.println("Moving backward");
+    }
+    else if (command == "Neutral") {
+      leftMotor.writeMicroseconds(1500);
+      rightMotor.writeMicroseconds(1500);
+      Serial.println("Stopped");
+    }
+    else {
+      Serial.println("Unknown command");
+    }
+  }
 }
